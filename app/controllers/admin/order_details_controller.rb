163e9c88876aa9cef22c78cad95c5aaa6404c2c3
EC2 Default User <ec2-user@ip-172-31-33-6.ap-northeast-1.compute.internal>
2022-11-17 05:00:28 +0000

    [Add]顧客側・管理者側

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,7 +2,9 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
   def update
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
     @order_detail.update(order_detail_params)
+
     redirect_to admin_order_path(@order.id)
   end
 
