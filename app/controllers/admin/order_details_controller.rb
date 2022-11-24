b2bee6e42a19b7ca1ee12b4fc882c643bbfa465d
EC2 Default User <ec2-user@ip-172-31-33-6.ap-northeast-1.compute.internal>
2022-11-24 08:33:54 +0000

    [Add]管理者側

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,10 +3,17 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
-
+    @order_details = @order.order_details
     @order_detail.update(order_detail_params)
 
-    redirect_to admin_order_path(@order.id)
+    if params[:order_detail][:making_status] == "in_production"
+      @order.update(status: "in_production")
+    end
+
+    if @order_details.count == @order_details.where(making_status: "production_complete").count
+      @order.update(status: "preparing_delivery")
+    end
+    redirect_to admin_order_path(@order)
   end
 
   private
